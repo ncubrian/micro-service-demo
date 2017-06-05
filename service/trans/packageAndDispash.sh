0687b0274941dab9d3e27b07f45fd216023ad681
Rogers <rogers.chen@cardinfolink.com>
2017-06-05 18:17:38 +0800

    use host of my own

--- service/trans/packageAndDispash.sh
+++ service/trans/packageAndDispash.sh
@@ -1,5 +1,5 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o transService main.go
-scp transService vagrant@192.168.99.40:/opt/service/
-scp transService vagrant@192.168.99.50:/opt/service/
-scp transService vagrant@192.168.99.60:/opt/service/
+scp transService root@192.168.99.11:/opt/service/
+scp transService root@192.168.99.22:/opt/service/
+scp transService root@192.168.99.33:/opt/service/
 rm ./transService
